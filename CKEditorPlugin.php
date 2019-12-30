75090bb06fd68434d29d8620d059095f7f4245fb
R-J <robin.jurinka@gmx.net>
2019-12-30 21:28:50 +0100

    First commit: basic functionality

---
+++ CKEditorPlugin.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace RJPlugins;
+
+use Gdn;
+use Gdn_Plugin;
+
+class CKEditorPlugin extends Gdn_Plugin {
+    /**
+     *  Run on startup to init sane config settings and db changes.
+     *
+     *  @return void.
+     */
+    public function setup() {
+        $this->structure();
+    }
+
+    /**
+     *  Create tables and/or new columns.
+     *
+     *  @return void.
+     */
+    public function structure() {
+        // $Configuration['Garden']['InputFormatter'] = 'Rich'; // Html, BBCode, Markdown, Text, Rich
+        // $Configuration['Garden']['MobileInputFormatter'] = 'Rich';
+        Gdn::config()->saveToConfig('Garden.InputFormatter', 'Html');
+    }
+
+
+    public function base_render_before($sender) {
+        $sender->addJsFile('ckeditor.js', 'plugins/rj-ckeditor');
+        $sender->addJsFile('init.js', 'plugins/rj-ckeditor');
+    }
+
+    public function gdn_form_beforeBodyBox_handler($sender, $args) {
+        echo '<div class="container-ckeditor"></div>';
+    }
+}
---
+++ js/init.js
@@ -0,0 +1,18 @@
+$( document ).on( 'contentLoad', function( e ) {
+    const bodyBox = $( '.BodyBox,.js-bodybox', e.target )[0];
+    $( bodyBox ).addClass('Hidden');
+    const container = $( '.container-ckeditor', e.target )[0];
+
+    ClassicEditor
+        .create( container )
+        .then( editor => {
+            console.log( editor );
+            editor.setData( bodyBox.value );
+            editor.model.document.on( 'change:data', () => {
+                bodyBox.value = editor.getData();
+            } );
+        } )
+        .catch( error => {
+            console.error( error );
+        } );
+});
