fa7fd7d917f6b8f24d785bb01bc5ecb66e5554cb
R-J <robin.jurinka@gmx.net>
2019-12-31 00:02:15 +0100

    Change how editor is connected to textarea

--- js/init.js
+++ js/init.js
@@ -1,12 +1,21 @@
-$( document ).on( 'contentLoad', function( e ) {
-    const bodyBox = $( '.BodyBox,.js-bodybox', e.target )[0];
-    // $( bodyBox ).addClass('Hidden');
-    const container = $( '.container-ckeditor', e.target )[0];
+document.addEventListener( 'X-ClearCommentForm', function( e ){
+    clearInput( e.target );
+}, false);
 
+function clearInput( el ) {
+    const ckeditor = el.querySelector( '.container-editor' );
+}
+
+$( document ).on( 'contentLoad', function( e ) {
+    replaceEditor( e );
+});
+
+function replaceEditor( e ) {
+    const container = $( '.BodyBox,.js-bodybox', e.target )[0];
+    const bodyBox = container;
     ClassicEditor
         .create( container )
         .then( editor => {
-            console.log( editor );
             editor.setData( bodyBox.value );
             editor.model.document.on( 'change:data', () => {
                 bodyBox.value = editor.getData();
@@ -15,4 +24,4 @@ $( document ).on( 'contentLoad', function( e ) {
         .catch( error => {
             console.error( error );
         } );
-});
+}
